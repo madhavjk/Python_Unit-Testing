e334b097caed74d57d9a8e9baf0e6d9697fb1148
Madhav <msharma043510@gmail.com>
2020-11-10 03:43:49 +0530

    Initial commit

---
+++ Introduction/Your first unit test/test_hello_someone.py
@@ -0,0 +1,7 @@
+import unittest
+
+from hello_someone import hello_someone
+
+class TestHelloWorld(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertEqual(hello_someone("World"), "Hello, World!")
